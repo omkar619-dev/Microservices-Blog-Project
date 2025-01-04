0a6c8da6103d43d058820bc858820b1538454adf
Omkar <omkarshendge619@gmail.com>
2025-01-04 17:51:39 +0530

    microservices initial setup

---
+++ comments/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const {randomBytes} = require('crypto');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {   // new route
+  const id = randomBytes(4).toString("hex");
+  const {content} = req.body;
+  const comments = commentsByPostId[req.params.id] || [];   // new line
+  comments.push({id, content, status: "pending"});   // new line
+  commentsByPostId[req.params.id] = comments;
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id,
+      content,
+      postId: req.params.id,
+      status: "pending"   // new line
+    }
+  });  // new line
+  return res.status(201).send(comments);
+});
+app.post('/events', async (req, res) => {
+  console.log('Received Event:', req.body.type);
+  const {type,data} = req.body
+  if(type==='CommentModerated'){
+    const {postId,id,status,content} = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events',{
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    });
+  }
+  res.send({});
+});  // new route
+
+
+app.listen(4001, () => {
+  console.log("Listening on 4001");
+});
---
+++ posts/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const {randomBytes} = require('crypto');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const axios = require('axios');
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const  posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', async (req, res) => {      // new route
+  const id = randomBytes(4).toString('hex');
+  const {title} = req.body;
+  posts[id] = {
+    id, title
+  };
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id, title
+    }
+  });  // new line
+  return res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event:', req.body.type);
+  res.send({});
+});  // new route
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
